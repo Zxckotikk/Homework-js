2d9b3d70503c42a992548d411cc2842d98505a04
Zxckotikk <125144342+Zxckotikk@users.noreply.github.com>
2025-01-25 14:14:32 +0200

    Homework

--- js/01-home.js
+++ js/01-home.js
@@ -6,3 +6,20 @@ const isStudent = "true"
 console.log(isStudent);
 const myString = "我做我想做的事，因為我可能很快就會死去"
 console.log(myString);
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+const yourName = prompt("Ведіть ваше ім'я");
+const welcome = alert("Вітаю!" + yourName + "!!");
+console.log(yourName);
+const confirms = confirm("Підтвердження");
+if (confirms === true) {
+    alert("Дякую за підтвердження!")
+} else {
+    alert("Дію відмінено!")
+}
+
+
+
