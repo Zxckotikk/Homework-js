f89c998871beca7aff9fd1d1b44a7690e536c4f4
Zxckotikk <125144342+Zxckotikk@users.noreply.github.com>
2025-01-25 11:55:49 +0200

    homework

---
+++ js/01-home.js
@@ -0,0 +1,8 @@
+const age = "14"
+console.log(age);
+const myName = "shivaqx"
+console.log(myName);
+const isStudent = "true"
+console.log(isStudent);
+const myString = "我做我想做的事，因為我可能很快就會死去"
+console.log(myString);
