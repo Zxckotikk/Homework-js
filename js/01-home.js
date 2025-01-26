eacf0617adfbdafa37c530956cb579ae5c71fb35
Zxckotikk <125144342+Zxckotikk@users.noreply.github.com>
2025-01-26 10:14:13 +0200

    Update 01-home.js

--- js/01-home.js
+++ js/01-home.js
@@ -1,19 +1,26 @@
 const age = "14"
 console.log(age);
+
 const myName = "shivaqx"
 console.log(myName);
+
 const isStudent = "true"
 console.log(isStudent);
+
 const myString = "我做我想做的事，因為我可能很快就會死去"
 console.log(myString);
+
 let myNumber = 10;
 myNumber = myNumber + 10;
 console.log(myNumber);
+
 const myNull = null;
 console.log(myNull);
+
 const yourName = prompt("Ведіть ваше ім'я");
 const welcome = alert("Вітаю!" + yourName + "!!");
 console.log(yourName);
+
 const confirms = confirm("Підтвердження");
 if (confirms === true) {
     alert("Дякую за підтвердження!")
