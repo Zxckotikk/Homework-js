d2ccf1a836a206304953a7b13857e55eebae6aab
Zxckotikk <125144342+Zxckotikk@users.noreply.github.com>
2025-02-01 09:41:29 +0200

    Update 01-home.js

--- js/01-home.js
+++ js/01-home.js
@@ -4,7 +4,7 @@ console.log(age);
 const myName = "shivaqx"
 console.log(myName);
 
-const isStudent = "true"
+const isStudent = true;
 console.log(isStudent);
 
 const myString = "我做我想做的事，因為我可能很快就會死去"
